2ce1ae35c2d7866ad01c2c9e97b21b664157c625
DylanOpenDev <dylantodev@gmail.com>
2020-10-22 12:50:45 +0800

    refactor: Clean components

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,51 +1,37 @@
-
 import React from 'react'
 import { useHistory } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { Button, Popover, Menu, Badge } from 'antd'
 import popoverContent from './HeaderPopoverContent'
 import walletBox from './HeaderWalletBox'
 import i18n from '../../utils/i18n'
 import MetaMaskpng from '../../assets/img/wallet/metamask.png'
 import outlined from '../../assets/img/outlined.png'
-import { connect } from 'react-redux'
-import {
-  HeaderBox,
-  HeaderPanel,
-  HeaderLogoBox, 
-  MenuLiText,
-  HeaderMeta,
-  UserMeta,
-} from './styled'
-import { 
-  Button, 
-  Popover, 
-  Menu, 
-  Badge 
-} from 'antd'
+import { HeaderBox, HeaderPanel, HeaderLogoBox, MenuLiText, HeaderMeta, UserMeta } from './styled'
 
 const mapStateToProps = (state: any) => {
   return {
     isConnectWallet: state.isConnectWallet,
-    address: state.address
+    address: state.address,
   }
 }
 
-export default connect(mapStateToProps)(
-  (props: any) => {
-    const isConnectWallet = props.isConnectWallet || false
-    const address = props.isConnectWallet || false
+export default connect(mapStateToProps)((props: any) => {
+  const isConnectWallet = props.isConnectWallet || false
+  const address = props.isConnectWallet || false
 
-    const history = useHistory()
+  const history = useHistory()
 
-    const truncatureStr = (str: string): string => {
-      return str?.length >= 5 ? (str.slice(0, 5) + '...' + str.slice(-5)) : ''
-    }
+  const truncatureStr = (str: string): string => {
+    return str?.length >= 5 ? `${str.slice(0, 5)}...${str.slice(-5)}` : ''
+  }
 
-    return (
-      <HeaderBox className="header-box">
-        <HeaderPanel>
-          <div className="panel-nav">
-            <HeaderLogoBox>CKB DEX</HeaderLogoBox>
-            <Menu mode="horizontal" onClick={e => history.push(`/${e.key}`)}>
+  return (
+    <HeaderBox className="header-box">
+      <HeaderPanel>
+        <div className="panel-nav">
+          <HeaderLogoBox>CKB DEX</HeaderLogoBox>
+          <Menu mode="horizontal" onClick={e => history.push(`/${e.key}`)}>
             <Menu.Item key="trade">
               <MenuLiText>{i18n.t(`header.Trade`)}</MenuLiText>
             </Menu.Item>
@@ -56,42 +42,34 @@ export default connect(mapStateToProps)(
               <MenuLiText>{i18n.t(`header.Match`)}</MenuLiText>
             </Menu.Item>
           </Menu>
-          </div>
-          <HeaderMeta id="headerMeta">
-            { !isConnectWallet ? (
-              <Popover 
-                placement="bottomRight"  
-                trigger="click" 
-                content={ popoverContent } 
+        </div>
+        <HeaderMeta id="headerMeta">
+          {!isConnectWallet ? (
+            <Popover placement="bottomRight" trigger="click" content={popoverContent}>
+              <Button className="collect-btn">{i18n.t('header.wallet')}</Button>
+            </Popover>
+          ) : (
+            <>
+              <UserMeta>
+                <img src={MetaMaskpng} alt="metaMask" />
+                {truncatureStr(address)}
+              </UserMeta>
+              <Popover
+                placement="bottomRight"
+                title=""
+                overlayClassName="no-arrorPoint"
+                trigger="click"
+                content={walletBox}
               >
-                <Button className="collect-btn">
-                  {i18n.t('header.wallet')}
-                </Button>
+                <Badge count="">
+                  <img src={outlined} alt="account" className="account-btn" />
+                  {/* <Button className="account-btn" icon={ <AlignCenterOutlined /> }></Button> */}
+                </Badge>
               </Popover>
-              ) : (
-                <>
-                  <UserMeta>
-                    <img src={ MetaMaskpng } alt="metaMask" />
-                    {truncatureStr(address)}
-                  </UserMeta>
-                  <Popover 
-                    placement="bottomRight" 
-                    title="" 
-                    overlayClassName="no-arrorPoint"
-                    trigger="click" 
-                    content={ walletBox }
-                  >
-                    <Badge count="">
-                      <img src={outlined} alt="account" className="account-btn" />
-                      {/* <Button className="account-btn" icon={ <AlignCenterOutlined /> }></Button> */}
-                    </Badge>
-                  </Popover>
-                </>
-              )
-            }
-          </HeaderMeta>
-        </HeaderPanel>
-      </HeaderBox>
-    )
-  }
-)
+            </>
+          )}
+        </HeaderMeta>
+      </HeaderPanel>
+    </HeaderBox>
+  )
+})
--- src/components/Popver/index.tsx
+++ src/components/Popver/index.tsx
@@ -1,25 +1,8 @@
-import React, { useState } from 'react'
-import {
-	Popover
-} from 'antd'
+import React from 'react'
+import { Popover } from 'antd'
 
-const Popver = ({
-  visible,
-  className
-}: {
-  visible: boolean
-  className: string
-  handleClose?: () => void
-}) => {
-	
-  return (
-		<Popover
-			visible={ visible }
-			className={ className }
-		>
-			
-		</Popover>
-  )
+const Popver = ({ visible, className }: { visible: boolean; className: string; handleClose?: () => void }) => {
+  return <Popover visible={visible} className={className} />
 }
 
 export default Popver
