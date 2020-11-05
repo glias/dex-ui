d4086eb8d62693c73181954208c9e27e829e49ef
coldsnap666 <realyuche@gmail.com>
2020-11-06 01:46:00 +0800

    refactor: remove unused deps

--- src/context/reducers/index.tsx
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import trace from './trace'
-import wallet from './wallet'
-
-export default combineReducers({
-  trace,
-  wallet,
-})
--- src/index.tsx
+++ src/index.tsx
@@ -5,9 +5,7 @@ import 'ant-design-icons/dist/anticons.min.css'
 import styled from 'styled-components'
 import 'antd/dist/antd.css'
 import './utils/i18n'
-import { Provider } from 'react-redux'
 import Routers from './routes'
-import store from './context/store'
 import { WalletContainer } from './containers/wallet'
 import { OrderContainer } from './containers/order'
 
@@ -18,14 +16,12 @@ const AppDiv = styled.div`
 `
 
 ReactDOM.render(
-  <Provider store={store}>
-    <WalletContainer.Provider>
-      <OrderContainer.Provider>
-        <AppDiv>
-          <Routers />
-        </AppDiv>
-      </OrderContainer.Provider>
-    </WalletContainer.Provider>
-  </Provider>,
+  <WalletContainer.Provider>
+    <OrderContainer.Provider>
+      <AppDiv>
+        <Routers />
+      </AppDiv>
+    </OrderContainer.Provider>
+  </WalletContainer.Provider>,
   document.getElementById('root'),
 )
