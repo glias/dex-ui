e38e5c549d2a6d2edd47f2861e4dd04677bc5030
homu-chain <dex.ckb.fe@gmail.com>
2020-11-16 21:03:46 +0800

    chore: supported monorepo

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,21 @@
-/* eslint-disable react-hooks/rules-of-hooks */
-const { useBabelRc, override } = require('customize-cra')
+/* eslint-disable import/no-extraneous-dependencies */
+const { override, useBabelRc } = require('customize-cra')
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 
-const config = override(useBabelRc())
+const tsconfigPath = config => {
+  // Remove the ModuleScopePlugin which throws when we try to import something
+  // outside of src/.
+  config.resolve.plugins.pop()
 
-module.exports = config
+  // Resolve the path aliases.
+  config.resolve.plugins.push(new TsconfigPathsPlugin())
+
+  // Let Babel compile outside of src/.
+  const tsRule = config.module.rules[2].oneOf[1]
+  tsRule.include = undefined
+  tsRule.exclude = /node_modules/
+
+  return config
+}
+
+module.exports = override(useBabelRc(), tsconfigPath)
