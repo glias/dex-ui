07946e8acea5538328c9554f876987ffa4b8c057
coldsnap666 <realyuche@gmail.com>
2020-11-21 17:26:50 +0800

    refactor: swap UI

--- src/pages/Trade/components/TradeOrderTable/styled.tsx
+++ src/pages/Trade/components/TradeOrderTable/styled.tsx
@@ -1,8 +1,24 @@
 import styled from 'styled-components'
 
+export const BID_CONFIRM_COLOR = '#ff9a6f'
+export const ASK_CONFRIM_COLOR = '#72d1a4'
+
+export const BID_RECEIVE_COLOR = '#FCF0E6'
+export const BID_RECEIVE_BORDER = '#FAE2D0'
+
+export const ASK_RECEIVE_COLOR = '#E9F6F1'
+export const ASK_RECEIVE_BORDER = '#D8EBD9'
+
+export interface ContainerProps {
+  isBid: boolean
+}
+
 export const InputNumberContainer = styled.div`
   .ant-form {
     background: #fff;
+    display: flex;
+    flex-direction: column;
+    height: 100%;
     .ant-form-item {
       label {
         font-size: 16px;
@@ -15,6 +31,14 @@ export const InputNumberContainer = styled.div`
         color: #000000;
         height: 34px;
       }
+      .receive {
+        border: solid 1px ${(props: ContainerProps) => (props.isBid ? BID_RECEIVE_BORDER : ASK_RECEIVE_BORDER)};
+        background-color: ${(props: ContainerProps) => (props.isBid ? BID_RECEIVE_COLOR : ASK_RECEIVE_COLOR)};
+        cursor: default;
+        input {
+          cursor: text;
+        }
+      }
       .ant-input-affix-wrapper-lg {
         padding: 10px;
       }
@@ -24,12 +48,20 @@ export const InputNumberContainer = styled.div`
       input[type='number']::-webkit-outer-spin-button {
         margin: 0;
       }
+      &.submit {
+        margin-top: auto;
+        margin-bottom: 0;
+      }
     }
     .ant-input-suffix {
       font-size: 12px;
       line-height: 14px;
       color: #888888;
       align-items: flex-end;
+    }
+    .ant-divider-horizontal {
+      margin: 10px 0;
+      border-top-color: #e1e1e1;
     }
   }
   .ant-input-affix-wrapper,
@@ -41,40 +73,7 @@ export const InputNumberContainer = styled.div`
 `
 
 export const OrderTableContainer = styled(InputNumberContainer)`
-  .receiver-box {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0 10px;
-    color: rgba(81, 119, 136, 1);
-    .receiver-ckb {
-      font-weight: 500px;
-      font-size: 28px;
-      color: rgba(0, 0, 0, 1);
-    }
-  }
-  .submit-item {
-    padding: 0 !important;
-    margin: 0;
-    height: 60px;
-    text-align: center;
-    .submit-btn {
-      padding: 0;
-      font-size: 17px;
-      font-weight: 500;
-      height: 60px;
-      line-height: 60px;
-      width: 100%;
-      line-height: 60px;
-      color: rgba(0, 106, 151, 1);
-      text-align: center;
-      &[disabled] {
-        background: rgba(241, 241, 241, 1);
-        border-bottom-right-radius: 10px;
-        border-bottom-left-radius: 10px;
-      }
-    }
-  }
+  height: 100%;
 `
 
 export const OrderSelectPopover = styled.div`
