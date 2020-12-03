d83cb4c83a441bd8a959fc1a1bc6506289ba7958
homu-chain <dex.ckb.fe@gmail.com>
2020-12-03 23:39:00 +0800

    refactor: remove unused code

--- src/components/Header/styled.tsx
+++ src/components/Header/styled.tsx
@@ -73,9 +73,12 @@ export const HeaderLogoBox = styled.h1`
 `
 
 export const UserMeta = styled.div`
+  border-radius: 10px;
+  padding: 8px 10px;
   display: inline-block;
-  font-size: 12px;
-  margin-right: 10px;
+  background: #fff;
+  color: #6b70e0;
+  cursor: pointer;
   > img {
     width: 20px;
     height: 20px;
@@ -115,168 +118,20 @@ export const ButtonWalletSvgBox = styled.div`
 `
 
 export const HeaderMeta = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  .more-btn {
-    border-radius: 10px;
-    background: rgba(0, 106, 151, 1);
-    color: #fff;
-    margin-left: 5px;
-    width: 28px;
-    height: 28px;
-    text-align: center;
+  .popover-wallet {
+    box-shadow: 3px 3px 8px 0 rgba(0, 0, 0, 0.08);
+  }
+
+  .ant-popover-inner {
+    border-radius: 16px;
+  }
+
+  .ant-popover-inner-content {
     padding: 0;
   }
-  .ant-popover-content {
-    overflow: hidden;
+
+  .btn-connect {
     border-radius: 10px;
-  }
-  .btn-meta {
-    width: 28px;
-    height: 28px;
-    padding: 4px 7px;
-    background: rgba(0, 106, 151, 1);
-    color: #fff;
-    border-radius: 10px;
-    border: 1px solid rgba(0, 106, 151, 1);
-  }
-  .ant-popover-content {
-    padding: 0;
-    border-radius: 10px;
-    .sidebar-content {
-      width: 150px;
-      display: flex;
-      flex-direction: column;
-      .sidebar-title {
-        line-height: 40px;
-        height: 40px;
-        text-align: center;
-        background: rgba(0, 106, 151, 1);
-        color: #fff;
-      }
-      button {
-        margin: 5px 0;
-      }
-    }
-  }
-  .popover-wallet-box {
-    width: 300px;
-    .wallet-list {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      height: 40px;
-      color: rgba(136, 136, 136, 1);
-      padding: 0 10px;
-      span {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-      img {
-        width: 15px;
-        height: 15px;
-        margin-left: 10px;
-      }
-    }
-    .balances {
-      h4 {
-        color: rgba(0, 0, 0, 1);
-        font-size: 17px;
-      }
-      .divider {
-        border-top: 1px solid rgba(171, 209, 225, 1);
-      }
-      ul > li {
-        margin: 10px 0;
-        padding: 0 10px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        .logo img {
-          width: 40px;
-        }
-        .wallet-info {
-          flex: 1;
-          padding: 0 10px;
-          .ckb-main-box {
-            display: flex;
-            justify-content: space-between;
-            border-bottom: 1px solid rgba(171, 209, 225, 1);
-            margin-bottom: 5px;
-            padding-bottom: 5px;
-            .ckb-name {
-              font-weight: 600;
-            }
-            .ckb-price {
-              text-align: right;
-            }
-          }
-          .ckb-use {
-            display: flex;
-            justify-content: space-between;
-            font-size: 12px;
-          }
-          .balance-item {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            .balance-name {
-              color: rgba(0, 0, 0, 1);
-              font-size: 14px;
-              font-weight: 600;
-            }
-            .balance-price {
-              display: flex;
-              flex-direction: column;
-              align-items: center;
-              text-align: right;
-              color: rgba(102, 102, 102, 1);
-              font-size: 12px;
-              .total-num {
-                font-size: 14px;
-                font-weight: 600;
-                color: rgba(0, 0, 0, 1);
-              }
-            }
-          }
-        }
-        .explorer img {
-          width: 15px;
-        }
-      }
-    }
-  }
-  .collect-btn {
-    color: #fff;
-    background-color: #006a97;
-    font-weight: 500;
-    border-color: #006a97;
-    border-radius: 10px;
-    &:disabled {
-      background-color: #006a97;
-      &:hover {
-        color: #fff;
-      }
-    }
-  }
-  .account-btn {
-    width: 15px;
-    display: inline-block;
-    margin-left: 10px;
-    cursor: pointer;
-  }
-  .wallet-box {
-    font-size: 13px;
-    img {
-      width: 25px;
-      margin-right: 10px;
-    }
-    button {
-      border-color: rgba(0, 106, 151, 1);
-      border-radius: 10px;
-      margin: 0 10px;
-    }
+    color: #6b70e0;
   }
 `
