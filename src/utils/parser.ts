0830f47de4912a39f9e4a2cb4f90bf5270369872
coldsnap <realyuche@gmail.com>
2020-12-05 13:23:32 +0800

    refactor: change order book

--- src/utils/parser.ts
+++ src/utils/parser.ts
@@ -57,7 +57,12 @@ export const parseOrderRecord = ({
   )
 
   const turnoverRate = new BigNumber(turnover_rate)
-  const isClaimable = turnoverRate.isGreaterThan(0.999)
+  const isClaimable = turnoverRate.isGreaterThan(0.999) && turnoverRate.isLessThan(1)
+
+  if (isClaimable) {
+    // eslint-disable-next-line no-debugger
+    debugger
+  }
 
   return {
     key,
