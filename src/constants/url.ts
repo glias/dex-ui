ea4c66adfc8b9e609da5f707290a1b03cc671c8b
coldsnap666 <realyuche@gmail.com>
2020-12-09 19:31:20 +0800

    refactor: move etherscan and readme url to env

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -2,4 +2,5 @@ export const EXPLORER_URL = process.env.REACT_APP_EXPLORER_URL!
 export const CKB_NODE_URL = process.env.REACT_APP_CKB_NODE_URL!
 export const CKB_INDEXER_URL = process.env.REACT_APP_CKB_INDEXER_URL!
 export const EXPLORER_API = process.env.REACT_APP_EXPLORER_API!
-export const ETHER_SCAN_URL = 'https://ropsten.etherscan.io/'
+export const ETHER_SCAN_URL = process.env.REACT_APP_ETHER_SCAN_URL!
+export const README_URL = process.env.REACT_APP_README_URL!
