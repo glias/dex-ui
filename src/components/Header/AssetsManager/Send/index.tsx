484ce5c099f8260d68cb0c2fe12c21293b3cd559
homu-chain <dex.ckb.fe@gmail.com>
2020-12-10 16:29:05 +0800

    fix: trigger form validation when the 'max' clicked

--- src/components/Header/AssetsManager/Send/index.tsx
+++ src/components/Header/AssetsManager/Send/index.tsx
@@ -88,7 +88,6 @@ export const Send: React.FC = () => {
   const debouncedValidateInput = useCallback(
     debounce(async () => {
       await form.validateFields(['amount', 'to'])
-
       setInputAllValidated(true)
     }, 200),
     [form, setInputAllValidated],
