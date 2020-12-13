2d8676fee98710c0cdde6f734f389b942eec4310
coldsnap666 <realyuche@gmail.com>
2020-12-13 20:59:25 +0800

    refactor: change header style

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -4,3 +4,4 @@ export const CKB_INDEXER_URL = process.env.REACT_APP_CKB_INDEXER_URL!
 export const EXPLORER_API = process.env.REACT_APP_EXPLORER_API!
 export const ETHER_SCAN_URL = process.env.REACT_APP_ETHER_SCAN_URL!
 export const README_URL = process.env.REACT_APP_README_URL!
+export const FAUCET_URL = process.env.REACT_APP_FAUCET_URL!
