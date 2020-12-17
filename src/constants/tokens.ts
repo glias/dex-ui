8e7583f0690b2119cb51b09d6b7342c4e8697be6
homu-chain <dex.ckb.fe@gmail.com>
2020-12-17 13:33:10 +0800

    feat: update ckb token icons

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -2,7 +2,7 @@ import dai from '../assets/img/token/dai.png'
 import eth from '../assets/img/token/eth.png'
 import usdt from '../assets/img/token/usdt.png'
 import glias from '../assets/img/token/glias.png'
-import ckb from '../assets/img/token/ckb.png'
+import ckb from '../assets/img/token/ckb.svg'
 import usdc from '../assets/img/token/usdc.png'
 
 export const TOKEN_LOGOS = new Map<string, string>()
