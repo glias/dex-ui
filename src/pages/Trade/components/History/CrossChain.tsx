fd8c27874116b349d5d2f90e5caab605eff03e62
coldsnap666 <realyuche@gmail.com>
2021-01-20 15:44:23 +0800

    fix: fix typo (again)

--- src/pages/Trade/components/History/CrossChain.tsx
+++ src/pages/Trade/components/History/CrossChain.tsx
@@ -208,7 +208,7 @@ const OrderModal = ({
             {secondDescription}
             <Tooltip
               title={`This step may take 5-15 minutes. We need to wait for the confirmation of 15 blocks on the ${
-                !isLock ? 'Ethereum' : 'Nervos'
+                isLock ? 'Ethereum' : 'Nervos'
               } to ensure the security.`}
             >
               <i className="ai-question-circle-o" />
